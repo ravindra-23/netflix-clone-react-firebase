3b3e21da7d5dbdb1e5be8bad53e32a2bb29f230e
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-15 22:30:18 +0530

    added Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header, Info, Accordion, Footer } from '../components'
+
+const Home = () => {
+  return (
+    <>
+        <Header />
+        <Info />
+        <Accordion />
+        <Footer />
+    </>
+  )
+}
+
+export default Home
