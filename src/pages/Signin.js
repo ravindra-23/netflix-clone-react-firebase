0528e9513462029f64320306286bdba41676cfed
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-16 12:18:30 +0530

    added sigin page

---
+++ src/pages/Signin.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer, SigninForm, Header } from '../components'
+
+const Signin = () => {
+  return (
+    <>
+      <Header>
+        <SigninForm />
+      </Header>
+      <Footer />
+    </>
+  )
+}
+
+export default Signin
