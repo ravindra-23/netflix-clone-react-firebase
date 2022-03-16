8fb938b3ad1d44a007100ed8ac843a1bcbdacf41
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-16 12:18:37 +0530

    added signup page

---
+++ src/pages/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer, Header, SignupForm } from '../components'
+
+const Signup = () => {
+  return (
+    <>
+        <Header>
+            <SignupForm />
+        </Header>
+        <Footer />
+    </>
+  )
+}
+
+export default Signup
