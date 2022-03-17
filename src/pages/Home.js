c8317cf3967cc7c012af3ce52735877b9e612078
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-17 11:41:07 +0530

    refactor home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Header, Info, Accordion, Footer, Feature } from '../components'
+import { Container, Link, Logo, ButtonLink } from '../components/Header/styles'
 
 const Home = () => {
   return (
     <>
-        <Header>
+        <Header bg={true}>
+          <Container>
+            <Link to='/'>
+              <Logo src='../images/misc/logo.svg' alt='Netflix'/>
+            </Link>
+            <ButtonLink to='/signin'>Sign In</ButtonLink>
+          </Container>
           <Feature />
         </Header>
         <Info />
