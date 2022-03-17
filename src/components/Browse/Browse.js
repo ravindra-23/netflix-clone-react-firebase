4e5bbf411b1cac0e69bb64e47f185c518641092c
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-17 20:27:49 +0530

    added cards component to browse

--- src/components/Browse/Browse.js
+++ src/components/Browse/Browse.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Movies } from '../../Axios/requests'
-import { Header, SearchBar } from '../index'
+import { Header, SearchBar, Card } from '../index'
 import app from '../../Firebase/firebase'
 import { getAuth } from 'firebase/auth'
 import { Container, Dropdown, Feature, FeatureCallOut, Group, Link, Logo, Picture, PlayButton, Profile, Text } from '../Header/styles'
@@ -14,47 +13,57 @@ const BrowseContent = () => {
   
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const data  = await axios.get(requests.fetchNetflixOriginals)
-      setBannerMovie(data.data.results[Math.floor(Math.random() * data.data.results.length - 1)])
+    const fetchData = async () => {
+      const {data}  = await axios.get(requests.fetchNetflixOriginals)
+      setBannerMovie(data.results[Math.floor(Math.random() * data.results.length - 1)])
       return data
     }
-    fetchMovies()
+    fetchData()
   }, [])
-  console.log(bannerMovie);
  
   return (
-    <Header src={bannerMovie.backdrop_path} dontShowOnSmallViewPort bg={false}>
-      <Container>
-        <Group>
-          <Logo to='/home' src='../images/misc/logo.svg' alt='Netflix' />
-          <Link>Films</Link>
-          <Link>Tv Shows</Link>
-        </Group>
+    <>
+      <Header src={bannerMovie.backdrop_path} dontShowOnSmallViewPort bg={false}>
+        <Container>
+          <Group>
+            <Logo to='/home' src='../images/misc/logo.svg' alt='Netflix' />
+            <Link>Films</Link>
+            <Link>Tv Shows</Link>
+          </Group>
 
-        <Group>
-          <SearchBar />
-          <Profile>
-            <Picture src='../images/users/1.png' />
-            <Dropdown>
-              <Group>
-                <Picture src='../images/users/1.png' />
-                <Link>{user.displayName}</Link>
-              </Group>
-              <Group>
-                <Link>Sign out</Link>
-              </Group>
-            </Dropdown>
-          </Profile>
-        </Group>
-      </Container>
+          <Group>
+            <SearchBar />
+            <Profile>
+              <Picture src='../images/users/1.png' />
+              <Dropdown>
+                <Group>
+                  <Picture src='../images/users/1.png' />
+                  {/* <Link>{user.displayName}</Link> */}
+                </Group>
+                <Group>
+                  <Link>Sign out</Link>
+                </Group>
+              </Dropdown>
+            </Profile>
+          </Group>
+        </Container>
 
-      <Feature>
-        <FeatureCallOut>Watch {bannerMovie.name}</FeatureCallOut>
-        <Text>{bannerMovie.overview}</Text>
-        <PlayButton>Play</PlayButton>
-      </Feature>
-    </Header>
+        <Feature>
+          <FeatureCallOut>Watch {bannerMovie.name}</FeatureCallOut>
+          <Text>{bannerMovie.overview}</Text>
+          <PlayButton>Play</PlayButton>
+        </Feature>
+      </Header>
+
+      <Card title='Top Rated' id='TR' fetchUrl={requests.fetchTopRated} />
+      <Card title='Action' id='AM' fetchUrl={requests.fetchActionMovies} />
+      <Card title='Trending' id='TN' fetchUrl={requests.fetchTrending} />
+      <Card title='Romantic' id='RM' fetchUrl={requests.fetchRomanceMovies} />
+      <Card title='Comedy' id='CM' fetchUrl={requests.fetchComedyMovies} />
+      <Card title='Horror' id='HM' fetchUrl={requests.fetchHorrorMovies} />
+      <Card title='Documentries' id='DM' fetchUrl={requests.fetchDocumentaries} />
+    
+    </>
   )
 }
 
