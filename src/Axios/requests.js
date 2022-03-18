90966b7b545c41ebe0b150c601599f6848dee21f
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-18 13:58:28 +0530

    added tv shows requests

--- src/Axios/requests.js
+++ src/Axios/requests.js
@@ -1,6 +1,7 @@
 export const API_KEY = 'a1ece1c64e9a9e900d0e50c3fe6609da';
 
 const requests = {
+    // Movies
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
@@ -9,6 +10,14 @@ const requests = {
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
+    // Tv shows
+
+    fetchTrendingTV: `/trending/tv/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginalsTV: `/discover/tv?api_key=${API_KEY}&with_networks=213&language=en-US`,
+    fetchAiringTodayTV: `/tv/airing_today?api_key=${API_KEY}&language=en-US`,
+    fetchPopularTV: `/tv/popular?api_key=${API_KEY}&language=en-US`,
+    fetchTopRatedTV: `/tv/top_rated?api_key=${API_KEY}&language=en-US`,
 }
 
 export default requests
