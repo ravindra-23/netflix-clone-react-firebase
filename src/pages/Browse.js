92e3cdff0a4965e515a1a03c297da1f3dbeaa49d
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-18 15:56:31 +0530

    added browse page

---
+++ src/pages/Browse.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BrowseContent, Footer} from '../components'
+
+const Browse = () => {
+  return (
+    <>
+      <BrowseContent />
+      <Footer />
+    </>
+  )
+}
+
+export default Browse
