840ac875ff6e767b3a2fa6cc77ea07c07275f427
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-20 14:20:09 +0530

    styles

--- src/components/Browse/BrowserHeader/styles.js
+++ src/components/Browse/BrowserHeader/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Header = styled.section`
     display: flex;
     flex-direction: column;
-    background: url(${({ src }) => (src ? `https://image.tmdb.org/t/p/original${src}` : '../images/misc/home-bg.jpg')}) top left / cover no-repeat;
+    background: url(${({ src }) => (src && `https://image.tmdb.org/t/p/original${src}`)}) top left / cover no-repeat;
     
     @media (max-width: 768px) {
         ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
---
+++ src/components/SearchResults/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    margin-top: 6rem;
+    width: 70%;
+    margin: 0 auto;
+`;
+
+export const Inner = styled.div`
+    padding: 2rem;
+    text-align: center;
+`;
+
+export const Text = styled.h2`
+    font-size: 1.5rem;
+    color: #fff;
+`;
+
+export const QueryText = styled.span`
+    font-weight: bold;
+`;
+
+export const Valid = styled.p`
+    font-size: 1.5rem;
+    margin-top: 1rem;
+`;
+
+export const Results = styled.div`
+    margin-top: 10rem;
+`;
+
+
+
